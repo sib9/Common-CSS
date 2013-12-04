20f5d260f54a0f9c94b4ee8861eecc832e725268
JerryZhang <chinajiezhang@gmail.com>
2013-12-04 23:32:42 +0800

    add python script for Convert to a real html_format for we can test easy

--- gen.sh
+++ gen.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Tanky Woo @ 2013-11-28 21:20:26
 
-markdown_py -x fenced_code -x 'codehilite(guess_lang=False)' index.md -f index.html
+html_name=index.html
+
+markdown_py -x fenced_code -x 'codehilite(guess_lang=False)' index.md -f $html_name
+
+python ConvertToRealHtmlFormat.py $html_name
