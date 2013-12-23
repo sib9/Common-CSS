235d241e2373786bfb875e2c22fea7ad1027065c
JerryZhang <chinajiezhang@gmail.com>
2013-12-23 21:21:54 +0800

    delete useless file

--- gen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Tanky Woo @ 2013-11-28 21:20:26
-
-html_name=index.html
-
-markdown_py -x fenced_code -x 'codehilite(guess_lang=False)' index.md -f $html_name
-
-python ConvertToRealHtmlFormat.py $html_name
